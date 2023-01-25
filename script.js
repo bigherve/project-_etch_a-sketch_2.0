740449b550568a60a7ba2720972e8c1f947f9b75
bigherve <hervekunda@gmail.com>
2023-01-25 19:38:30 +0100

    Add a hover effect so that the grid divs change color when your mouse passes over them, leaving a pixelated trail

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ function makeGrid(rows, columns) {
     container.style.setProperty('--grid-columns', columns);
     for (let i = 0; i < rows * columns; i++) {
         let div = document.createElement('div');
-        div.textContent = i + 1;
         div.className = 'grid-square';
         container.appendChild(div);
     }
 }
 
-makeGrid(16, 16);
+makeGrid(40, 40);
+
+const fun = document.querySelectorAll('.grid-square');
+
+fun.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        e.target.style.background = 'hotpink';
+    });
+});
